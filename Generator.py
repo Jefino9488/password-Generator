e90604531e97e58533852a7bb46672b2f8d02c6c
Jefino 1537 <jefinojacob1537@gmail.com>
2023-12-01 18:35:46 +0530

    added copy button few ui stuff

--- Generator.py
+++ Generator.py
@@ -3,6 +3,7 @@ import string
 import tkinter as tk
 from tkinter import IntVar
 from tkinter import messagebox
+import pyperclip
 
 
 class PasswordGenerator:
@@ -34,29 +35,69 @@ class PasswordGeneratorUI:
 
     def create_widgets(self):
         length_label = tk.Label(self.root, text="Password Length:", bg='white')
-        length_label.grid(row=0, column=0, padx=10, pady=10)
+        length_label.grid(row=0, column=0, columnspan=1, padx=10, pady=10)
 
-        length_slider = tk.Scale(self.root, from_=6, to=20, orient=tk.HORIZONTAL, variable=self.length_var, bg='white')
-        length_slider.grid(row=0, column=1, padx=10, pady=10)
+        length_label = tk.Label(self.root, text="|", bg='white')
+        length_label.grid(row=0, column=3, columnspan=1, padx=10, pady=10)
+
+        length_slider = tk.Scale(self.root, from_=6, to=30, orient=tk.HORIZONTAL, length=150, variable=self.length_var, bg='white')
+        length_slider.grid(row=0, column=1, padx=5, pady=5)
+
+        length_label = tk.Label(self.root, text="|", bg='white')
+        length_label.grid(row=1, column=3, columnspan=1, padx=10, pady=10)
 
         upper_check = tk.Checkbutton(self.root, text="Uppercase", variable=self.upper_var, bg='white')
-        upper_check.grid(row=1, column=0, padx=10, pady=5, sticky=tk.W)
+        upper_check.grid(row=1, column=0, padx=20, pady=5, sticky=tk.W)
 
         lower_check = tk.Checkbutton(self.root, text="Lowercase", variable=self.lower_var, bg='white')
-        lower_check.grid(row=2, column=0, padx=10, pady=5, sticky=tk.W)
+        lower_check.grid(row=2, column=0, padx=20, pady=5, sticky=tk.W)
+
+        length_label = tk.Label(self.root, text="|", bg='white')
+        length_label.grid(row=2, column=3, columnspan=1, padx=10, pady=10)
 
         number_check = tk.Checkbutton(self.root, text="Numbers", variable=self.number_var, bg='white')
-        number_check.grid(row=3, column=0, padx=10, pady=5, sticky=tk.W)
+        number_check.grid(row=3, column=0, padx=20, pady=5, sticky=tk.W)
+
+        length_label = tk.Label(self.root, text="|", bg='white')
+        length_label.grid(row=3, column=3, columnspan=1, padx=10, pady=10)
 
         special_check = tk.Checkbutton(self.root, text="Special Characters", variable=self.special_var, bg='white')
-        special_check.grid(row=4, column=0, padx=10, pady=5, sticky=tk.W)
+        special_check.grid(row=4, column=0, padx=20, pady=5, sticky=tk.W)
+
+        length_label = tk.Label(self.root, text="|", bg='white')
+        length_label.grid(row=4, column=3, columnspan=1, padx=10, pady=10)
 
         generate_button = tk.Button(self.root, text="Generate Password", command=self.generate_password, bg='orange',
                                     fg='white')
-        generate_button.grid(row=5, column=0, columnspan=2, pady=10)
+        generate_button.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
+
+        length_label = tk.Label(self.root, text="|", bg='white')
+        length_label.grid(row=5, column=3, columnspan=1, padx=10, pady=10)
 
         self.password_display = tk.Entry(self.root, width=30, state='readonly', font=('Courier', 12))
-        self.password_display.grid(row=6, column=0, columnspan=2, pady=10)
+        self.password_display.grid(row=7, column=0, columnspan=2,padx=10, pady=10)
+
+        copy_button = tk.Button(self.root, text="Copy to Clipboard", command=self.copy_to_clipboard, bg='blue',
+                                fg='white')
+        copy_button.grid(row=7, column=4, columnspan=1, padx=10, pady=10)
+
+        length_label = tk.Label(self.root, text="_____________________________", bg='white')
+        length_label.grid(row=6, column=0, padx=10, pady=0)
+        length_label = tk.Label(self.root, text="_____________________________", bg='white')
+        length_label.grid(row=6, column=1, padx=0, pady=0)
+        length_label = tk.Label(self.root, text="_____", bg='white')
+        length_label.grid(row=6, column=3, padx=0, pady=0)
+        length_label = tk.Label(self.root, text="_____________________________", bg='white')
+        length_label.grid(row=6, column=4, padx=0, pady=0)
+
+        length_label = tk.Label(self.root, text="|", bg='white')
+        length_label.grid(row=7, column=3, columnspan=1, padx=10, pady=10)
+
+    def copy_to_clipboard(self):
+        password = self.password_display.get()
+        if password:
+            pyperclip.copy(password)
+            tk.messagebox.showinfo("Copy to Clipboard", "Password copied to clipboard!")
 
     def generate_password(self):
         length = int(self.length_var.get())
